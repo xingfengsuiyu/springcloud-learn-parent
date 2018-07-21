05ea6de91c15d63edc6f1f5b0848d860968e1527
zca <xingfengsuiyu@hotmail.com>
2018-07-21 19:09:20 +0800

    SpringCloud Finchley.RELEASE 学习案例

--- feign/src/main/java/com/zc/feign/ApiService.java
+++ feign/src/main/java/com/zc/feign/ApiService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient( value = "eureka-client1",fallback = ApiServiceError.class)
+@FeignClient( value = "eureka-client",fallback = ApiServiceError.class)
 public interface ApiService {
 	@RequestMapping( value = "/index", method = RequestMethod.GET)
 	String index();
